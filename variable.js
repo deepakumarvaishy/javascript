824e2ba4e377d2f4b983993b7c78ec949a8ac3d6
deepakumarvaishy <deepak9161gupta@gmail.com>
2025-04-25 13:00:03 +0530

    variable.js

--- variable.js
+++ variable.js
@@ -1,1 +1,8 @@
 console.log("deepak");
+const accountid = 1234
+var accountname = "deepak gupta "
+let accountbalance = 10000
+let accountpassword = "deepak@1233"
+
+console.log(accountid);
+console.table([accountid, accountname, accountbalance, accountpassword]);
